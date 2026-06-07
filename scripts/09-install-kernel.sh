c46e3cf15e93ab7db25ea84f141051ef995a43dc
GengWei1997 <gw19970326@Gmail.com>
2026-06-07 13:23:36 +0800

    Package the device GPU firmware into the initramfs.
    Fixed the error where the kernel failed to find the firmware during startup.

--- scripts/09-install-kernel.sh
+++ scripts/09-install-kernel.sh
@@ -20,6 +20,28 @@ chroot rootdir dpkg -i /tmp/firmware-xiaomi-raphael.deb
 
 rm rootdir/tmp/*-xiaomi-raphael.deb
 
+echo "[$(date +'%Y-%m-%d %H:%M:%S')] [09]   └─ 创建自定义 initramfs firmware hook..."
+cat > rootdir/etc/initramfs-tools/hooks/custom-qcom-firmware << 'EOF'
+#!/bin/sh
+PREREQ=""
+prereqs() {
+    echo "$PREREQ"
+}
+case $1 in
+prereqs)
+    prereqs
+    exit 0
+    ;;
+esac
+
+. /usr/share/initramfs-tools/hook-functions
+
+copy_exec /lib/firmware/qcom/a630_sqe.fw
+copy_exec /lib/firmware/qcom/a640_gmu.bin
+copy_exec /lib/firmware/qcom/sm8150/Xiaomi/raphael/a640_zap.mbn
+EOF
+chmod +x rootdir/etc/initramfs-tools/hooks/custom-qcom-firmware
+
 echo "[$(date +'%Y-%m-%d %H:%M:%S')] [09]   └─ 更新 initramfs..."
 chroot rootdir update-initramfs -c -k all
 
